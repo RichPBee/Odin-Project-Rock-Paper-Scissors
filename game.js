fbdf48b138207840dc6ddaad262a26777dd104c4
ArrBee <ri.balfourth@gmail.com>
2022-08-21 20:20:38 +0100

    Built majority of the game logic:
    function to create AI choice. function for player choice.
    function to check winner. currently debugging function
    that checks if player input is valid. else statement needed.

---
+++ game.js
@@ -0,0 +1,84 @@
+function getComputerChoice(num) {
+    num = Math.round(num);
+    return (num == 0) ? "Rock"
+        : num == 1 ? "Paper"
+        : num == 2 ? "Scissors"
+        : "Not an answer."
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "Rock" :
+                return "It\'s a draw! Rock versus Rock!";
+            case "Paper" :
+                return "You Lose! Paper beats Rock.";
+            case "Scissors" :
+                return "You Win! Rock beats Scissors.";
+        }
+    } else if (playerSelection === "scissors") {
+        switch (computerSelection) {
+            case "Rock" :
+                return "You Lose! Rock beats Scissors.";
+            case "Paper" :
+                return "You Win! Scissors beat Paper.";
+            case "Scissors" :
+                return "It\'s a draw! Scissors versus Scissors!";             
+        }
+    } else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "Rock" :
+                return "You Win! Paper beats Rock.";
+            case "Paper" :
+                return "It\'s a draw! Paper versus Paper!";
+            case "Scissors" :
+                return "You Lose! Scissors beat Paper!";
+        }
+    } else {
+        console.log()
+    }
+}
+
+function checkPlayerChoice(val) {
+    if (val !== "rock" && val !== "paper" && val !== "scissors") {
+        let newChoice = prompt(`Rock, Paper, Scissors. 
+        Make your choice: `); 
+        newChoice = newChoice.toLowerCase();
+        checkPlayerChoice(newChoice);
+    } 
+
+    const currentChoice = val;
+    return currentChoice;
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let draws = 0;
+    
+    for (let i = 0; i < 5; i ++){
+        let playerChoice = prompt(`Rock, Paper, Scissors. 
+        Make your choice: `); 
+        playerChoice = playerChoice.toLowerCase();
+        playerChoice = checkPlayerChoice(playerChoice);
+        let computerChoice = getComputerChoice(Math.random() * 2);
+        let result = playRound(playerChoice, computerChoice);
+        if (result.includes("Win")) {
+            playerScore++;
+        } else if (result.includes("Lose")) {
+            computerScore++;
+        } else {
+            draws++;
+        }
+        console.log(result);
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`You win. You scored ${playerScore} points, the computer only scored ${computerScore}.`);
+    } else if (playerScore < computerScore) {
+        console.log(`You lose. You scored ${playerScore} points, the computer scored ${computerScore}.`);
+    } else {
+        console.log(`It's a draw! You and the computer both scored ${playerScore} points.`);
+    }
+}
+game()
