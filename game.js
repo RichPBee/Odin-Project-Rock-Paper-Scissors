8d57e12dc5297257e6f57939dc937d79de86427a
ArrBee <ri.balfourth@gmail.com>
2022-08-28 01:50:59 +0100

    complete project.
    Forgot to commit along the way, as the project became more about
    learning DOM manipulation than the specifics of the project/
    design. Succesfully built RPS game to specification.

--- game.js
+++ game.js
@@ -1,3 +1,93 @@
+let counter = 0;
+let roundNumber = 0;
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+
+//This section of code is solely for DOM manipulation.
+const addScorebar = (winner) => {
+    const scorebar = document.createElement('li');
+    scorebar.setAttribute("class", "scorebar");
+    let scorecard = document.querySelector(winner);
+    scorecard.appendChild(scorebar);
+}
+
+const removeInfoText = () => {
+    const display = document.querySelector(".display");
+    const infoText = document.querySelectorAll(".info-text");
+    infoText.forEach(item => {
+        display.removeChild(item)  
+    })
+}
+
+const setUpDisplay = () => {
+    const playList = document.createElement("ul")
+    playList.setAttribute("class", "outcome-list");
+    const display = document.querySelector(".display");
+    for (let i = 0; i < 7; i ++) {
+        const play = document.createElement("li")
+        play.setAttribute("class", "outcome-bar")
+        play.setAttribute("id", `${i}`);
+        playList.append(play);
+    }
+    display.append(playList);
+}
+
+const clearDisplay = () => {
+    const outcomes = document.querySelectorAll(".outcome-bar");
+    outcomes.forEach(outcome => {
+        outcome.innerHTML = "";
+    })
+}
+
+const displayResult = (winResult) => {
+    let winnerText = document.createElement("h4");
+    let winBars = document.querySelectorAll(".outcome-bar")
+    winnerText.textContent = `Round ${roundNumber + 1}: ${winResult}`;
+    winBars[counter].append(winnerText);
+    
+}
+/* let numRounds;
+const roundSelectorBtns = document.querySelectorAll(".round-selector");
+roundSelectorBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let numRounds = (btn.id == "three") ? "three": "five";
+        removeInfoText()
+        return numRounds;
+    })
+}) */
+
+
+
+const choiceBtns = document.querySelectorAll(".choice-button");
+choiceBtns.forEach((btn => {
+    btn.addEventListener("click", () => {
+        let playerChoice =  (btn.id == "rock") ? "rock"
+            : (btn.id == "paper") ? "paper"
+            : (btn.id == "scissors") ? "scissors"
+            : "not a choice";
+        if (counter == 0){
+            removeInfoText();
+            setUpDisplay();
+        } else if (counter % 7 == 0) {
+            counter = 0;
+            clearDisplay();
+        }
+        game(playerChoice, getComputerChoice(Math.random() * 2));
+        return
+        })
+    }
+))
+
+const resetBtn = document.querySelector(".reset-button");
+resetBtn.addEventListener("click", () => {
+    alert("You have reset the page.")
+    location.reload();
+})
+
+
+
+// This section of the code deals with the game logic. 
 function getComputerChoice(num) {
     num = Math.round(num);
     return (num == 0) ? "Rock"
@@ -51,34 +141,37 @@ function checkPlayerChoice(choice) {
 
 
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let draws = 0;
-    
-    for (let i = 0; i < 5; i ++){
-        let playerChoice = prompt(`Rock, Paper, Scissors. 
-        Make your choice: `);
-        playerChoice = playerChoice.toLowerCase();
-        let checkedChoice = checkPlayerChoice(playerChoice);
-        let computerChoice = getComputerChoice(Math.random() * 2);
-        let result = playRound(checkedChoice, computerChoice);
-        if (result.includes("Win")) {
-            playerScore++;
-        } else if (result.includes("Lose")) {
-            computerScore++;
-        } else {
-            draws++;
-        }
-        console.log(result);
+function game(playerChoice, computerChoice){
+
+    let result = playRound(playerChoice, computerChoice)
+    if (result.includes("Win")) {
+        playerScore++;
+        addScorebar("#player");
+        displayResult(result);
+    } else if (result.includes("Lose")) {
+        computerScore++;
+        addScorebar("#cpu");
+        displayResult(result);
+    } else {
+        draws++;
+        displayResult(result);
     }
 
-    if (playerScore > computerScore) {
-        console.log(`You win. You scored ${playerScore} points, the computer only scored ${computerScore}.`);
+    console.log(result);
+
+
+    /*if (playerScore > computerScore) {
+        addScorebar("#player")
     } else if (playerScore < computerScore) {
-        console.log(`You lose. You scored ${playerScore} points, the computer scored ${computerScore}.`);
+        addScorebar("#cpu")
     } else {
-        console.log(`It's a draw! You and the computer both scored ${playerScore} points.`);
+        console.log(draws);
+    } */
+    counter ++;
+    roundNumber ++;
+    if(playerScore == 15 || computerScore == 15) {
+        alert("Maximum system score reached - resetting.")
+        location.reload();
     }
 }
-game()
+
